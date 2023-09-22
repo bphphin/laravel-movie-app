de98e7954ba67ae6b1fd6c5b2802090aa0f6dd26
bphphin <buingocphinn@gmail.com>
2023-09-22 20:14:53 +0700

    feat: call api genres

--- app/Http/Controllers/API/MovieController.php
+++ app/Http/Controllers/API/MovieController.php
@@ -9,11 +9,25 @@ class MovieController extends Controller
 {
     public function index() {
         $api_movie_popular = config('api_url.api_movie_popular');
+        $api_genre = config('api_url.api_genre');
         $token_movie = config('api_url.movie_token');
+
         $popularMovie = Http::withToken($token_movie)
         ->get($api_movie_popular)
         ->json()['results'];
-        // dump($popularMovie);
-        return view('movies.index',compact('popularMovie'));
+
+        $genreArray = Http::withToken($token_movie)
+        ->get($api_genre)
+        ->json()['genres'];
+
+        $genres = collect($genreArray)->mapWithKeys(function ($genre) {
+            return [
+                $genre['id'] => $genre['name']
+            ];
+        });
+        dump($genres);
+        dump($popularMovie);
+
+        return view('movies.index',compact('popularMovie','genres'));
     }
 }
