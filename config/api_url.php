562f244a2c0962aa004ab6dcd4472c8ee3e000af
bphphin <buingocphinn@gmail.com>
2023-09-22 22:40:35 +0700

    feat: config api detail movie

--- config/api_url.php
+++ config/api_url.php
@@ -1,13 +1,22 @@
 <?php
 return [
-
+    // APi hiển thị tất cả danh sách thuộc popular
     'api_movie_popular' => 'https://api.themoviedb.org/3/movie/popular',
 
+    // APi hiển thị thể loại
     'api_genre' => 'https://api.themoviedb.org/3/genre/movie/list',
 
+
+    // APi hiển thị danh sách movie đáng xem (now playing)
     'api_now_playing' => 'https://api.themoviedb.org/3/movie/now_playing',
 
 
+    // APi chi tiết movie
+    'api_movie_details' => 'https://api.themoviedb.org/3/movie/',
+
+
+
+    // Token call api
     'movie_token' => 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiODRlYjIyNjJhYWE3ZTU5MmZhYzNjN2MzZGM3ZTgxOSIsInN1YiI6IjY1MGQ2MmY0NGRhM2Q0MDBhZGM5YzcxNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.RJt7FUN9Pla-Wbmc_Yo9PZnpjgGCymsI94YkRr-X9OY',
 ];
 ?>
