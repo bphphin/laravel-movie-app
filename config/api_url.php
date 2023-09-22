2fbfd2313b929e84ccc8d7813a0c5bf76571fa7a
bphphin <buingocphinn@gmail.com>
2023-09-22 19:42:13 +0700

    feat: config url and token of movie api

---
+++ config/api_url.php
@@ -0,0 +1,8 @@
+<?php
+return [
+
+    'api_movie_popular' => 'https://api.themoviedb.org/3/movie/popular',
+
+    'movie_token' => 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiODRlYjIyNjJhYWE3ZTU5MmZhYzNjN2MzZGM3ZTgxOSIsInN1YiI6IjY1MGQ2MmY0NGRhM2Q0MDBhZGM5YzcxNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.RJt7FUN9Pla-Wbmc_Yo9PZnpjgGCymsI94YkRr-X9OY',
+];
+?>
