74c44a161b4407483037988e528ca9923d4f574c
bphphin <buingocphinn@gmail.com>
2023-09-22 19:43:14 +0700

    feat: route home

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\MovieController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('movies.show');
+Route::controller(MovieController::class)->group(function() {
+    Route::get('/', 'index')->name('movie.index');
 });
