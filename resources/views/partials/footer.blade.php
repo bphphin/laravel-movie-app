8053d6ae114e95dcb1150f5fe3042e16196fcac7
bphphin <buingocphinn@gmail.com>
2023-09-22 16:50:33 +0700

    UI

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="border border-t border-gray-800">
+    <div class="container mx-auto text-sm px-4 py-6">
+        <p class="text-center">Copy right by buingocphin &copy;</p>
+    </div>
+</footer>
