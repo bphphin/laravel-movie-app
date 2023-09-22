91acb8ec20289c99e0425fe7077425537f1ec2bc
bphphin <buingocphinn@gmail.com>
2023-09-22 20:26:29 +0700

    feat: config api url nowplaying list

--- config/api_url.php
+++ config/api_url.php
@@ -5,6 +5,7 @@ return [
 
     'api_genre' => 'https://api.themoviedb.org/3/genre/movie/list',
 
+    'api_now_playing' => 'https://api.themoviedb.org/3/movie/now_playing',
 
 
     'movie_token' => 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiODRlYjIyNjJhYWE3ZTU5MmZhYzNjN2MzZGM3ZTgxOSIsInN1YiI6IjY1MGQ2MmY0NGRhM2Q0MDBhZGM5YzcxNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.RJt7FUN9Pla-Wbmc_Yo9PZnpjgGCymsI94YkRr-X9OY',
